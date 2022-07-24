d3163388f5b886d468e422165cde1c1b6255e694
Pushpendra <pushpendra.patwal@gmail.com>
2022-07-24 21:46:47 +0800

    Model class Location

---
+++ QB2API/Model/QuestionModel.cs
@@ -0,0 +1,53 @@
+﻿namespace QB2API.Model
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Answer
+    {
+        public string ID { get; set; }
+        public string AnswerText { get; set; }
+        public string  AnswerImage { get; set; } 
+        public bool IsAnswer { get; set; }
+
+        
+        //client side support  
+        public bool IsChecked { get; set; }
+
+    }
+
+    public class QuestionTypes
+    {
+        public bool SingleChoice { get; set; }
+        public bool MultiChoice { get; set; }
+    }
+
+    public class QuestionModel
+    {
+        public Guid Guid { get; set; }
+        public string Topic { get; set; }
+        public List<string> Concept { get; set; }
+        public List<string> tags { get; set; }
+        public string QuestionText { get; set; }
+        public string QuestionImage { get; set; }
+        public QuestionTypes? QuestionTypes { get; set; }
+        public List<Answer> Answers { get; set; }
+        public int? Weight { get; set; }
+        public int? Difficulty { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public bool IsVerified { get; set; }
+
+        //client side support  
+        public bool  isSubmitted { get; set; }
+        public bool isCorrect { get; set; }
+
+
+    }
+
+    public class QuestionSetAPIResponseModel 
+       {
+        public Guid Guid { get; set; }
+        public List<QuestionModel> Questions { get; set; }
+    }
+
+
+}
